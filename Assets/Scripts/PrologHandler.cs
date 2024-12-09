41a9eb93f1eabd2f45df7dc8d8d56a41ed1c057b
Maximilian Amougou <m.amougou@str8labs.com>
2024-12-09 22:25:15 +0100

    refactor: replace Prolog scene with StartEndScreens and update UI elements, added epilog scene

--- Assets/Scripts/PrologHandler.cs
+++ Assets/Scripts/PrologHandler.cs
@@ -12,6 +12,14 @@ public class PrologHandler : MonoBehaviour
         " As punishment for your disobedience you are teleported to the prison planet Mars." +
         " To earn one of the few tickets back to earth you will have to prove yourself " +
         "in a series of fights. Good luck";
+    private string epilogTextWon = "Against all odds, you emerged victorious, earning your place back on Earth. " +
+        "The forbidden mask's burden is lifted, and your story becomes a testament to perseverance and triumph in the face of despair. " +
+        "Welcome home champion. Please take off the mask";
+    private string epilogTextLost = "Defeated, the weight of the forbidden mask crushes your spirit. " +
+        "Mars becomes your eternal prison, a desolate monument to failure. Yet in your loss, whispers of " +
+        "your struggle inspire others to defy the odds you could not";
+
+
 
     public TextMeshProUGUI uiText;
     public TTSSpeaker speaker;
@@ -19,17 +27,30 @@ public class PrologHandler : MonoBehaviour
     public void Start()
     {
         ttsScript = GetComponent<TTSScript>();
-        uiText.text = "|";
+        uiText.text = "";
         StartCoroutine(TypeTextWithBlink(prologText, uiText, () => { SceneManager.LoadScene(2); }));
     }
 
     private bool addBlink = false;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowEndScreen(Random.value > 0.5);
+        }
+    }
+
+    public void ShowEndScreen(bool won) // Call this method when the player wins or loses
+    {
+        StartCoroutine(TypeTextWithBlink(won ? epilogTextWon : epilogTextLost, uiText, () => { SceneManager.LoadScene(0); }));
+    }
+
     IEnumerator TypeTextWithBlink(string prologText, TextMeshProUGUI uiText, System.Action callback)
     {
         ttsScript.Speak(prologText.Replace("\n", " "), speaker);
         prologText += "...";
-
+        uiText.text = "|";
         yield return Blink(uiText, 6, 0.3f);
 
         foreach (char c in prologText)
