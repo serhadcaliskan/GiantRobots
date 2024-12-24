93ed3e1a5c2d69dfd9b9e59a30d05dc76c96940d
Maximilian Amougou <m.amougou@str8labs.com>
2024-12-24 14:22:57 +0100

    karma score influences dodge and disarm successrate

--- Assets/Scripts/Fight/PlayerStats.cs
+++ Assets/Scripts/Fight/PlayerStats.cs
@@ -5,32 +5,56 @@ using UnityEngine;
 public class PlayerStats : Stats
 {
     /// <summary>
+    /// Karma weight, how much karma influences the success rate of dodging and disarming
+    /// should be between 0 and 1!
+    /// </summary>
+    private float karmaWeight = 0.8f;
+
+    private int karmaScore;
+    /// <summary>
+    /// Karma score above 50 incresaes the probability of dodging and disarming, below decrerses it, 50 is neutral
+    /// </summary>
+    public int KarmaScore
+    {
+        get => karmaScore;
+        set => karmaScore = Mathf.Clamp(value, 0, 100);
+    }
+
+    public float KarmaScoreNormalized
+    {
+        get => Mathf.Clamp01(karmaScore / 100f);
+    }
+
+    /// <summary>
+    /// gets dodge success rate, influenced by karma
+    /// </summary>
+    public new float dodgeSuccessRate
+    {
+        get => Mathf.Clamp(base.dodgeSuccessRate + (KarmaScoreNormalized - 0.5f) * karmaWeight * (1 - base.dodgeSuccessRate), 0f, 1f);
+    }
+
+    /// <summary>
+    /// gets disarm success rate, influenced by karma
+    /// </summary>
+    public new float disarmSuccessRate
+    {
+        get
+        {
+            return Mathf.Clamp(base.disarmSuccessRate + (KarmaScoreNormalized - 0.5f) * karmaWeight * (1 - base.disarmSuccessRate), 0f, 1f);
+        }
+    }
+
+    /// <summary>
     /// Load the settings from PlayerPrefs, if not found, gets the default values
     /// </summary>
     public override void LoadGameSettings()
     {
-        if(PlayerPrefs.HasKey("lifePoints"))
-            lifePoints = PlayerPrefs.GetInt("lifePoints");
-        else lifePoints = 100;
-
-        if (PlayerPrefs.HasKey("shieldCount"))
-            shieldCount = PlayerPrefs.GetInt("shieldCount");
-        else shieldCount = 3;
-
-        if (PlayerPrefs.HasKey("shootDamage"))
-            shootDamage = PlayerPrefs.GetInt("shootDamage");
-        else shootDamage = 10;
-
-        if (PlayerPrefs.HasKey("loadCapacity"))
-            loadCapacity = PlayerPrefs.GetInt("loadCapacity");
-        else loadCapacity = 3;
-
-        if (PlayerPrefs.HasKey("dodgeSuccessRate"))
-            dodgeSuccessRate = PlayerPrefs.GetFloat("dodgeSuccessRate");
-        else dodgeSuccessRate = 0.5f;
-
-        if (PlayerPrefs.HasKey("disarmSuccessRate"))
-            disarmSuccessRate = PlayerPrefs.GetFloat("disarmSuccessRate");
-        else disarmSuccessRate = 0.5f;
+        lifePoints = PlayerPrefs.GetInt("lifePoints", 100);
+        shieldCount = PlayerPrefs.GetInt("shieldCount", 3);
+        shootDamage = PlayerPrefs.GetInt("shootDamage", 10);
+        loadCapacity = PlayerPrefs.GetInt("loadCapacity", 3);
+        base.dodgeSuccessRate = PlayerPrefs.GetFloat("dodgeSuccessRate", 0.5f);
+        base.disarmSuccessRate = PlayerPrefs.GetFloat("disarmSuccessRate", 0.5f);
+        karmaScore = PlayerPrefs.GetInt("karmaScore", 50);
     }
 }
